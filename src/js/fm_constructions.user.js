2281ba0fae6d2d48a5371df3d2c59f55422f3b0b
Blubb <->
2013-12-02 21:09:43 +0100

    User-Skript des Konstruktionsupdaters korrigiert.

--- src/js/fm_constructions.user.js
+++ src/js/fm_constructions.user.js
@@ -44,7 +44,7 @@
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
 function addJQuery(callback) {
   var script = document.createElement("script");
-  script.setAttribute("src", "//code.jquery.com/jquery-latest.min.js");
+  script.setAttribute("src", "//code.jquery.com/jquery-1.7.0.min.js");
   script.addEventListener('load', function() {
     var script = document.createElement("script");
     script.setAttribute("src", "//fm.omameier.net/js/fm_constructions.js?r="+Math.random());
